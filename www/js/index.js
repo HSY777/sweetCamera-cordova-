3f4dcbf854ce604b682b40c741aa75dcaa81a645
HONG <ongsy1828@naver.com>
2021-05-10 02:07:09 +0900

    commit on my *desktop* at *home* on *05/10*

--- www/js/index.js
+++ www/js/index.js
@@ -1,85 +1,32 @@
 var app = {
     startCameraAbove: function(){
-      CameraPreview.startCamera({x: 50, y: 50, width: 300, height: 500, toBack: true, previewDrag: false, tapPhoto: false});
-    },
-  
-    startCameraBelow: function(){
-      CameraPreview.startCamera({x: 50, y: 50, width: 300, height:300, camera: "front", tapPhoto: true, previewDrag: false, toBack: true});
+      CameraPreview.startCamera({x: 50, y: 50, width: 300, height: 500, toBack: false, previewDrag: false, tapPhoto: false});
     },
   
     stopCamera: function(){
       CameraPreview.stopCamera();
     },
-  
+
     takePicture: function(){
       CameraPreview.takePicture(function(imgData){
-        document.getElementById('originalPicture').src = 'data:image/jpeg;base64,' + imgData;
+        base64img = 'data:image/jpeg;base64,' + imgData //<img src="data:image/<이미지확장자>;base64,<data코드>")
+        document.getElementById('originalPicture').src = base64img;
+        console.log(base64img);
       });
     },
-  
-    switchCamera: function(){
-      CameraPreview.switchCamera();
-    },
-  
-    show: function(){
-      CameraPreview.show();
-    },
-  
-    hide: function(){
-      CameraPreview.hide();
-    },
-  
-    changeColorEffect: function(){
-      var effect = document.getElementById('selectColorEffect').value;
-      CameraPreview.setColorEffect(effect);
-    },
-  
-    changeFlashMode: function(){
-      var mode = document.getElementById('selectFlashMode').value;
-      CameraPreview.setFlashMode(mode);
-    },
-  
-    changeZoom: function(){
-      var zoom = document.getElementById('zoomSlider').value;
-      document.getElementById('zoomValue').innerHTML = zoom;
-      CameraPreview.setZoom(zoom);
-    },
-  
-    changePreviewSize: function(){
-      window.smallPreview = !window.smallPreview;
-      if(window.smallPreview){
-        CameraPreview.setPreviewSize({width: 100, height: 100});
-      }else{
-        CameraPreview.setPreviewSize({width: window.screen.width, height: window.screen.height});
-      }
-    },
-  
-    showSupportedPictureSizes: function(){
-      CameraPreview.getSupportedPictureSizes(function(dimensions){
-        dimensions.forEach(function(dimension) {
-          console.log(dimension.width + 'x' + dimension.height);
-        });
-      });
-    },
-  
+
     init: function(){
       document.getElementById('startCameraAboveButton').addEventListener('click', this.startCameraAbove, false);
       document.getElementById('stopCameraButton').addEventListener('click', this.stopCamera, false);
-  
-      if(navigator.userAgent.match(/Android/i)  == "Android"){
-        document.getElementById('zoomSlider').addEventListener('change', this.changeZoom, false);
-      }else{
-        document.getElementById('androidOnly').style.display = 'none';
-      }
-  
-      window.smallPreview = false;
-  
-      // legacy - not sure if this was supposed to fix anything
-      //window.addEventListener('orientationchange', this.onStopCamera, false);
+      document.getElementById('takePictureButton').addEventListener('click', this.takePicture, false);
+
+      CameraPreview.startCamera({x: 50, y: 50, width: 300, height: 500, toBack: false, previewDrag: false, tapPhoto: false});
     }
-  };
+  }; 
   
   document.addEventListener('deviceready', function(){	
     app.init();
   }, false);
+
+
   
